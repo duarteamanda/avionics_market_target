c6a22f8eaaf03cef447c4f1f4bd234d561a15c14
duarteamanda <sousa.duarteamanda@gmail.com>
2025-11-26 12:34:04 +1000

    Script updated

--- scripts/faa_master.py
+++ scripts/faa_master.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import os
 import geopandas as gpd
 import matplotlib.pyplot as plt
@@ -9,7 +8,7 @@ import matplotlib.patches as mpatches
 raw_path = "data/raw/faa/MASTER.txt"
 processed_dir = "data/processed"
 output_file = f"{processed_dir}/faa_master.csv"
-shapes_folder = "data/shapes"
+shapes_country = "data/shapes/countries"
 
 os.makedirs(processed_dir, exist_ok=True)
 master = pd.read_csv(raw_path, dtype=str)
@@ -18,7 +17,7 @@ print(f"Total records: {len(master)}")
 
 df = pd.read_csv(output_file, dtype=str)
 
-# Standardising
+# Standardisation
 str_cols = df.select_dtypes(include='object').columns
 df[str_cols] = df[str_cols].apply(lambda col: col.str.strip())
 df.replace('', pd.NA, inplace=True)
@@ -44,15 +43,19 @@ print(f"All N-NUMBER values unique? {is_unique}")
 df.drop(df[df['TYPE AIRCRAFT'].isin(['7', '8'])].index, inplace=True)
 print(f"Records after dropping Weight-shift-control and Powered Parachute from Type Aircraft: {len(df)}")
 
-# Aggregate aircraft counts by country
-# ------------------------------
+us_df = df[df['COUNTRY'] == 'US'].copy()
+missing_states = us_df['STATE'].isna().sum()
+print(f'Missing States: {missing_states}')
+
+us_counts = df[df['COUNTRY']=='US'].groupby('STATE').size().reset_index(name='aircraft_count')
+
+# Aircraft counts by country
 country_counts = df.groupby('COUNTRY').size().reset_index(name='aircraft_count')
 
 # Manual ISO mapping for FAA-specific codes
 manual_iso_map = {
     'AN': 'ANT',  # Netherlands Antilles
     'RQ': 'PRI',  # Puerto Rico
-    # Add more if needed
 }
 
 def iso2_to_iso3_fixed(code):
@@ -70,14 +73,12 @@ if not missing_iso.empty:
     print("Warning: Some countries could not be converted to ISO3 codes")
     print(missing_iso)
 
-# ------------------------------
 # Load shapefile
-# ------------------------------
-shp_files = [f for f in os.listdir(shapes_folder) if f.endswith(".shp")]
+shp_files = [f for f in os.listdir(shapes_country) if f.endswith(".shp")]
 if not shp_files:
-    raise ValueError(f"No shapefiles found in {shapes_folder}")
+    raise ValueError(f"No shapefiles found in {shapes_country}")
 
-shapefile_path = os.path.join(shapes_folder, shp_files[0])
+shapefile_path = os.path.join(shapes_country, shp_files[0])
 world = gpd.read_file(shapefile_path)
 
 # Merge aircraft counts into shapefile GeoDataFrame
@@ -85,17 +86,15 @@ shapefile_iso_col = 'ISO_A3' if 'ISO_A3' in world.columns else world.columns[0]
 world = world.merge(country_counts, how='left', left_on=shapefile_iso_col, right_on='iso3')
 world['aircraft_count'] = world['aircraft_count'].fillna(0)
 
-#Defining range
+#Define range
 bins = [1, 50, 1000, world['aircraft_count'].max()+1]
 labels = [1, 2, 3]
 
 world['category'] = pd.cut(world['aircraft_count'], bins=bins, labels=labels, include_lowest=True)
 
-# Defining colour
 color_map = {1: '#dbe9f7', 2: '#4292c6', 3: '#08306b'}
 world['color'] = world['category'].map(color_map)
 
-# Country without aircraft
 world['color'] = world['color'].astype(str).replace('nan', '#ffffff')
 
 # Plot
@@ -109,9 +108,11 @@ legend_handles = [
 ]
 
 ax.legend(handles=legend_handles, title='Aircraft Count', loc='lower right', bbox_to_anchor=(1.05, 0.5))
-
 ax.set_title('FAA - World Registered Aircraft', fontsize=16)
-plt.subplots_adjust(bottom=0.05)
-fig.text(0.5, 0.03, 'Counts include only aircraft registered with the U.S. FAA', ha='center', fontsize=10)
+plt.subplots_adjust(bottom=0.01)
+fig.text(0.5, 0.03,
+         'Counts include only aircraft registered with the U.S. FAA. Source Last updated: Tuesday, May 13, 2025.',
+         ha='center',
+         fontsize=10)
 ax.axis('off')
 plt.show()
